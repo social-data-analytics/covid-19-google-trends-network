b8adfd78bf1d55ea12c7e421e80a944974ec3482
Nick H. T. Lai <nicklai@ust.hk>
2023-06-26 16:05:15 +0800

    Initial commit: add graphic scripts

---
+++ scripts/information/covid_info.py
@@ -0,0 +1,9 @@
+covid_events = {
+    "2020-01-31": "COVID-19 was declared as a \"Public Health Emergency of International Concern\"",
+    "2020-03-11": "COVID-19 was declared as a pandemic",
+    "2021-05-31": "Delta variant was named",
+    "2021-11-26": "Omicron variant was named"
+}
+
+if __name__ == '__main__':
+    raise "This is a info only Python file."
